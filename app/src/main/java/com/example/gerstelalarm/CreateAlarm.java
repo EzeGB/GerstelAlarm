c9c08dc28a97553f47fcc6c5b9e7247788dcfd8a
Zeke <ezequielgerstel777@gmail.com>
2025-01-23 13:30:55 -0400

    not satisfactory commit

--- app/src/main/java/com/example/gerstelalarm/CreateAlarm.java
+++ app/src/main/java/com/example/gerstelalarm/CreateAlarm.java
@@ -2,9 +2,8 @@ package com.example.gerstelalarm;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +11,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateAlarm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_alarm);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Intent received = getIntent();
-        String newName = received.getStringExtra("AlarmName");
-        if (newName!=null) {
-            ((Button) findViewById(R.id.button)).setText(newName);
-        }
+        Intent createAlarm = getIntent();
+        String name = createAlarm.getStringExtra("AlarmName");
+        ((EditText)findViewById(R.id.editTextText)).setText(name);
     }
 
-    public void goToCreate (View view){
-        Intent createAlarm = new Intent(this, CreateAlarm.class);
-        String name = ((Button)view).getText().toString();
-        createAlarm.putExtra("AlarmName", name);
-        startActivity(createAlarm);
+    public void returnToMain (View view){
+        Intent goBack = new Intent(this, MainActivity.class);
+        String newName = ((EditText)findViewById(R.id.editTextText)).getText().toString();
+        goBack.putExtra("AlarmName", newName);
+        startActivity(goBack);
     }
 }
